9aa30e1250980b86d6f2cd4b84e6322341d43735
mcc1461 <mcoskuncelebi@gmail.com>
2024-05-14 15:27:48 +0300

    feat: mccblog api initialization continued...

---
+++ server/index.js
@@ -0,0 +1,63 @@
+"use strict";
+/* -------------------------------------------------------
+                NODE EXPRESS |  BLOG API
+---------------------------------------------------------- */
+
+/*
+    cp .env-sample .env
+    npm init -y
+    npm i express dotenv mongoose express-async-errors
+    npm i morgan swagger-autogen swagger-ui-express redoc-express
+    mkdir logs
+    nodemon
+    https://www.toptal.com/developers/gitignore/   (node) 
+*/
+
+/* ------------------------------------------------------- */
+
+const express = require("express");
+const app = express();
+
+// Required Modules:
+
+// envVariables to process.env:
+require("dotenv").config();
+const PORT = process.env?.PORT || 8001;
+const HOST = process.env?.HOST || "127.0.0.1";
+
+/* ------------------------------------------------------- */
+// Configrations:
+
+// Connect to DB:
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
+
+/* ------------------------------------------------------- */
+
+// Accept JSON:
+app.use(express.json());
+
+// Middlewares:
+app.use(require("./src/middlewares/findSearchSortPage"))
+
+// HOMEPATH
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to BLOG API",
+    documents: {
+      swagger: "/documents/swagger",
+      redoc: "/documents/redoc",
+      json: "/documents/json",
+    },
+    user: req.user,
+  });
+});
+
+// Routes: index yazılmasına gerek yok.
+app.use(require("./src/routes"));
+
+// Error Handler
+app.use(require("./src/middlewares/errorHandler"));
+
+app.listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`));
---
+++ server/src/controllers/user.js
@@ -0,0 +1,110 @@
+"use strict";
+
+/* -------------------------------------------------------
+        NODE EXPRESS | BLOG API
+------------------------------------------------------- */
+
+const User = require("../models/user");
+
+module.exports = {
+  // List all users
+  list: async (req, res) => {
+    try {
+      const users = await User.find();
+      res.status(200).send({
+        error: false,
+        data: users,
+      });
+    } catch (error) {
+      res.status(500).send({
+        error: true,
+        message: "Error fetching users."
+      });
+    }
+  },
+
+  // Create a new user
+  create: async (req, res) => {
+    try {
+      const newUser = await User.create(req.body);
+      res.status(201).send({
+        error: false,
+        data: newUser,
+      });
+    } catch (error) {
+      res.status(400).send({
+        error: true,
+        message: "Error creating user."
+      });
+    }
+  },
+
+  // Read a user by ID
+  read: async (req, res) => {
+    try {
+      const user = await User.findOne({ _id: req.params.id });
+      if (user) {
+        res.status(200).send({
+          error: false,
+          data: user,
+        });
+      } else {
+        res.status(404).send({
+          error: true,
+          message: "User not found."
+        });
+      }
+    } catch (error) {
+      res.status(500).send({
+        error: true,
+        message: "Error fetching user."
+      });
+    }
+  },
+
+  // Update a user by ID
+  update: async (req, res) => {
+    try {
+      const updatedUser = await User.updateOne({ _id: req.params.id }, req.body, {
+        runValidators: true,
+      });
+      if (updatedUser.matchedCount === 0) {
+        res.status(404).send({
+          error: true,
+          message: "User not found."
+        });
+      } else {
+        const user = await User.findOne({ _id: req.params.id });
+        res.status(202).send({
+          error: false,
+          data: user,
+        });
+      }
+    } catch (error) {
+      res.status(500).send({
+        error: true,
+        message: "Error updating user."
+      });
+    }
+  },
+
+  // Delete a user by ID
+  delete: async (req, res) => {
+    try {
+      const result = await User.deleteOne({ _id: req.params.id });
+      if (result.deletedCount > 0) {
+        res.status(204).send();
+      } else {
+        res.status(404).send({
+          error: true,
+          message: "User not found."
+        });
+      }
+    } catch (error) {
+      res.status(500).send({
+        error: true,
+        message: "Error deleting user."
+      });
+    }
+  },
+};
